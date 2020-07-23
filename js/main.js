e1176eba1d83e5e79b36eca44246866813c7b470
Andro <chvertkini.andro@gmail.com>
2020-07-23 22:05:53 +0400

    Some practice

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    // slidesPerView: 1,
+    loop: true,
+    // slidesPerView: "auto",
+  
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    
+  });
