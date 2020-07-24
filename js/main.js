1f6bbb91e39243ea293b07333a937d39809aeb7f
Andro <chvertkini.andro@gmail.com>
2020-07-24 20:28:05 +0400

    Some practice

--- js/main.js
+++ js/main.js
@@ -23,3 +23,16 @@ var mySwiper = new Swiper('.swiper-container', {
 
     
   });
+
+
+  function ibg(){
+
+    let ibg=document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+    if(ibg[i].querySelector('img')){
+    ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+    }
+    }
+    }
+    
+    ibg();
